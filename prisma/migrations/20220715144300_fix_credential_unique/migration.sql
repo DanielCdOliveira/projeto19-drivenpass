0188450c41659ec2b66a564bfe80e018d82f0a0f
isabellacgmsa <isabellacgmsa1@gmail.com>
2022-08-08 18:28:42 -0300

    Preparando para revisão de código

--- prisma/migrations/20220715144300_fix_credential_unique/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "credentials_title_key";
-
--- DropIndex
-DROP INDEX "credentials_userId_key";
--- src/routers/alldataRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import verifyToken from "../middlewares/verifytoken.js";
-import { getAlldata } from "../controllers/alldataController.js";
-const alldataRouter = Router()
-alldataRouter.get("/alldata",verifyToken, getAlldata)
-
-
-export default alldataRouter
